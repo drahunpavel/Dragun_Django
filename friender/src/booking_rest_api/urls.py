5cae7efe0101705537cbf87997ab2d745a0ac99b
Paul Drahun <drahunpavel@gmail.com>
2024-05-26 00:26:23 +0300

    improved urlpatterns

--- friender/src/booking_rest_api/urls.py
+++ friender/src/booking_rest_api/urls.py
@@ -1,13 +1,17 @@
-from django.urls import URLPattern, path
+from django.urls import URLPattern, include, path
 
 from .views import GuestApiView, hello_world
 
 
+guests_patterns: list[URLPattern] = [
+    path('list', GuestApiView.as_view(), name='guest_list'),
+    path('create', GuestApiView.as_view(), name='guest_create'),
+    path('update', GuestApiView.as_view(), name='guest_update'),
+    path('delete/<int:pk>', GuestApiView.as_view(), name='guest_delete'),
+]
+
 
 urlpatterns: list[URLPattern] = [
     path('some_url_example', hello_world),
-    path('guests/list', GuestApiView.as_view(), name='guest_list'),
-    path('guests/create', GuestApiView.as_view(), name='guest_create'),
-    path('guests/update', GuestApiView.as_view(), name='guest_update'),
-    path('guests/delete/<int:pk>', GuestApiView.as_view(), name='guest_delete'),
+    path('guests/', include((guests_patterns, 'guests'))),
 ]
